f064f221590287c88e2260e6a1f964eb55c9d5eb
Mikhail Terentiev <anhungered@gmail.com>
2016-02-01 01:17:09 +0300

    Updated selectMatter func, fixed frames and navigation

--- tests/matterForms.js
+++ tests/matterForms.js
@@ -10,6 +10,9 @@ var driver = req.driver,
 var assert = req.assert,
     fs = req.fs;
 
+var arrChapters = ['chapter 7', 'chapter 13'];
+
+req.catchUncaughtExceptions();
 
 driver.manage().window().maximize();
 driver.manage().timeouts().implicitlyWait(2000);
@@ -17,14 +20,15 @@ driver.manage().timeouts().implicitlyWait(2000);
 
 req.authorize(test.env, test.login, test.password);
 req.closeTabs();
-req.selectMatter(test.selMatterType, test.selChapter);
+req.selectMatter(test.selMatterType, test.selChapter, test.selJurisdiction);
 
-//OFFICIAL FORMS
+
 driver.wait(until.elementLocated(nav.navBarManage));
 driver.findElement(nav.navBarManage).click();
 driver.findElement(nav.navBarManageMatterForms).click();
 driver.wait(until.elementLocated(By.xpath("//tr[@data-pe-action='B101_1215']")));
 
+//OFFICIAL FORMS
 driver.findElements(By.xpath("//article/table/tbody/tr/td/div[2]/table/tbody/tr[starts-with(@id, 'grid_')]")).then(function(formsCount) {
     console.log('There are ' + formsCount.length + ' official forms');
     for (var i = 1; i <= formsCount.length; i++) {
@@ -32,7 +36,7 @@ driver.findElements(By.xpath("//article/table/tbody/tr/td/div[2]/table/tbody/tr[
             formNumber = fnum
         });
         driver.findElement(By.xpath("//div[starts-with(@id, 'officialCaseFormsForms_')]/div/article/table/tbody/tr/td/div[2]/table/tbody/tr[starts-with(@id, 'grid_')][" + i + "]/td/a")).click();
-        driver.wait(until.elementLocated(By.xpath("//section/div/iframe")), 10000);
+        driver.wait(until.elementLocated(By.xpath("//div[@class='metro window-overlay']//section/div/iframe")), 10000);
         driver.switchTo().frame(0);
 
         driver.wait(until.elementLocated(By.xpath("//html/body/section/article")), 10000).then(function() {
@@ -68,8 +72,8 @@ driver.findElements(By.xpath("//div[starts-with(@id, 'localCaseFormsForms_')]//d
             formNumber = fnum
         });
         driver.findElement(By.xpath("//div[starts-with(@id, 'localCaseFormsForms_')]//div[2]//tr[starts-with(@id, 'grid_')][" + i + "]/td/a")).click();
-        driver.wait(until.elementLocated(By.xpath("//section/div/iframe")), 10000);
-        driver.switchTo().frame(0);
+        driver.wait(until.elementLocated(By.xpath("//div[@class='metro window-overlay']//section/div/iframe")), 10000);
+        driver.switchTo().frame(1);
 
         driver.wait(until.elementLocated(By.xpath("//html/body/section/article")), 10000).then(function() {
             console.log('Local form ' + formNumber + ' OK')
@@ -96,6 +100,7 @@ driver.findElements(By.xpath("//div[starts-with(@id, 'localCaseFormsForms_')]//d
 //PLANS
 driver.findElement(By.xpath("//li[starts-with(@aria-controls, 'plansCaseFormsForms_')]/a")).click().then(function() {
     driver.wait(until.elementLocated(By.xpath("//div[starts-with(@id, 'plansCaseFormsForms_')]/div/article/table/tbody/tr/td/div[1]")));
+    
     driver.findElements(By.xpath("//div[starts-with(@id, 'plansCaseFormsForms_')]/div/article/table/tbody/tr/td/div[2]/table/tbody/tr[starts-with(@id, 'grid_')][not(contains(@id, '_DXEmptyRow'))]")).then(function(formsCount) {
         console.log('There are ' + formsCount.length + ' plan forms');
         for (var i = 1; i <= formsCount.length; i++) {
@@ -103,8 +108,8 @@ driver.findElement(By.xpath("//li[starts-with(@aria-controls, 'plansCaseFormsFor
                 formNumber = fnum
             });
             driver.findElement(By.xpath("//div[starts-with(@id, 'plansCaseFormsForms_')]/div/article/table/tbody/tr/td/div[2]/table/tbody/tr[starts-with(@id, 'grid_')][" + i + "]/td/a")).click();
-            driver.wait(until.elementLocated(By.xpath("//section/div/iframe")), 10000);
-            driver.switchTo().frame(0);
+            driver.wait(until.elementLocated(By.xpath("//div[@class='metro window-overlay']//section/div/iframe")), 10000);
+            driver.switchTo().frame(2);
 
             driver.wait(until.elementLocated(By.xpath("//html/body/section/article")), 10000).then(function() {
                 console.log('Plan form ' + formNumber + ' OK')
@@ -115,7 +120,7 @@ driver.findElement(By.xpath("//li[starts-with(@aria-controls, 'plansCaseFormsFor
                         req.saveScreenshot('Plan form_' + formNumber + '_failed_' + req.currentDate().replace(/\//g, '') + '.png' );
                     }
                 }, function(err) {
-                    console.log(err)
+                    console.log(err.name + ' ' + err.message)
                 })
             });
 
@@ -123,9 +128,11 @@ driver.findElement(By.xpath("//li[starts-with(@aria-controls, 'plansCaseFormsFor
             driver.findElement(By.xpath("//div[2]/section[contains(@id, 'container_dialogContent-Form')]/div[2]/button")).click();
             driver.sleep(1000);
         };
+        
     }, function(err) {
         console.log(err)
     });
+    
 }, function(noPlan) {
     console.log('Chapter 7: No plans')
 });
