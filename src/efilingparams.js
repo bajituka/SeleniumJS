6d09700ceea2d2cb94ef98cc2e60639797214e08
Mikhail Terentiev <anhungered@gmail.com>
2016-02-10 02:37:40 +0300

    fixed county

--- src/efilingparams.js
+++ src/efilingparams.js
@@ -2,7 +2,7 @@ var By = require('selenium-webdriver').By;
 
 var illinois = By.xpath("//select[@id='stateId']/option[@value='14']"),
     georgia = By.xpath("//select[@id='stateId']/option[@value='11']"),
-    county = By.xpath("//select[@id='Case_CountyId']/option[not(@disabled)][not(@value='')]"), //random county
+    county = By.xpath("//select[@id='Case_CountyId']/option[not(@disabled='')][not(@value='')]"), //random county
     ilnb = By.xpath("//select[@id='District_Id']/option[@value='28']"),
     ilcb = By.xpath("//select[@id='District_Id']/option[@value='29']"),
     ilsb = By.xpath("//select[@id='District_Id']/option[@value='30']"),
