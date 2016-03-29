2314ab334b48dd49c3fe6b2a868d4254e51e505a
Mikhail Terentiev <anhungered@gmail.com>
2016-03-29 20:04:56 +0300

    added means test

--- src/petition/incomeAndExpenses.js
+++ src/petition/incomeAndExpenses.js
@@ -16,7 +16,7 @@ var incomeAndExpenses = function() {
     var budget = By.xpath("//a[text()='Budget']"),
         employmentDetails = By.xpath("//a[text()='Employment Details']"),
         incomeChanges = By.xpath("//a[text()='Income Changes']");
-        
+    /*    
     var household = By.xpath("//a[text()='Household']"),
         utilities = By.xpath("//a[text()='Utilities']"),
         personal = By.xpath("//a[text()='Personal']"),
@@ -25,9 +25,11 @@ var incomeAndExpenses = function() {
         otherRealPropExpenses = By.xpath("//a[text()='Other Real Property Expenses']"),
         otherExpenses = By.xpath("//a[text()='Other Expenses']"),
         additional = By.xpath("//div[starts-with(@id, 'expenses')]//a[text()='Additional']");
-        
+    */
     var details = By.xpath("//div[starts-with(@id, 'meansTest')]//a[text()='Details']"),
         income = By.xpath("//div[starts-with(@id, 'meansTest')]//a[text()='Income']");
+    
+    var saveBtn = By.xpath("//form[starts-with(@id, 'meansTestForm')]//button[@type='submit']");
     
     driver.wait(until.elementLocated(nav.navMatter.petition.incomeAndExpenses.self));
     driver.findElement(nav.navMatter.petition.incomeAndExpenses.self).click();
@@ -55,6 +57,17 @@ var incomeAndExpenses = function() {
     //driver.wait(until.elementLocated(By.xpath("//*[@id='expenseChanges' and @value='True']")));
     
     //means test
+    var projectedPlanPayment = By.xpath("//input[@id='modelObject_ProjectedPlanPayment' and not(@type='hidden')]"),
+        addBtn = By.xpath("//div[@id='btnMaritalAdjustments']//div[contains(@class, 'add-adjustment-button')]"),
+        description = By.xpath("//input[@id='maritalExclusions_0__Description']"),
+        percentage = By.xpath("//input[@id='maritalExclusions_0__Amount' and not(@type='hidden')]"),
+        comments = By.xpath("//textarea[@id='modelObject_Comment']"),
+        militaryService = By.xpath("//div[starts-with(@id, 'MeansTest_')]//div[contains(@class, 'panel-header')]"),
+        disabledVeteran = By.xpath("//input[@id='modelObject_DisabledVeteran']"),
+        reservist = By.xpath("//input[@id='modelObject_ReservistOrNationalGuard']"),
+        activeDuty = By.xpath("//input[@id='active-duty-check']"),
+        homelandDefense = By.xpath("//input[@id='homeland-check']");
+    
     driver.wait(until.elementLocated(nav.navMatter.petition.incomeAndExpenses.meansTest));
     driver.findElement(nav.navMatter.petition.incomeAndExpenses.meansTest).click();
     
@@ -62,6 +75,49 @@ var incomeAndExpenses = function() {
     driver.findElement(details).click();
     driver.wait(until.elementLocated(By.xpath("//div[starts-with(@id, 'meansTest')]//input[@id='Zip']")));
     
+    driver.findElement(projectedPlanPayment).clear();
+    driver.findElement(projectedPlanPayment).sendKeys('40.6');
+    
+    driver.findElement(addBtn).click();
+    driver.wait(until.elementIsVisible(driver.findElement(description)));
+    driver.findElement(description).sendKeys('Marital description');
+    driver.findElement(percentage).clear();
+    driver.findElement(percentage).sendKeys('50.4');
+    
+    driver.findElement(comments).sendKeys('These are some nice comments for Means Test');
+    
+    driver.findElement(militaryService).click();
+    
+    driver.wait(until.elementIsEnabled(driver.findElement(disabledVeteran)), 5000);
+    driver.findElement(disabledVeteran).click();
+    driver.wait(until.elementIsEnabled(driver.findElement(By.xpath("//input[@id='modelObject_DisabledVeteranActivity' and @value='True']"))), 5000);
+    driver.executeScript("arguments[0].scrollIntoView(true);", driver.findElement(By.xpath("//input[@id='modelObject_DisabledVeteranActivity' and @value='True']")));
+    driver.findElement(By.xpath("//input[@id='modelObject_DisabledVeteranActivity' and @value='True']")).click();
+    
+    driver.findElement(reservist).click();
+    driver.wait(until.elementIsEnabled(driver.findElement(activeDuty)), 5000);
+    driver.executeScript("arguments[0].scrollIntoView(true);", driver.findElement(activeDuty));
+    driver.findElement(activeDuty).click();
+    driver.wait(until.elementIsEnabled(driver.findElement(By.xpath("//input[@id='modelObject_RemainOnActiveDuty']"))), 5000);
+    driver.executeScript("arguments[0].scrollIntoView(true);", driver.findElement(By.xpath("//input[@id='modelObject_RemainOnActiveDuty']")));
+    driver.findElement(By.xpath("//input[@id='modelObject_RemainOnActiveDuty' and @value='False']")).click();
+    driver.wait(until.elementIsEnabled(driver.findElement(By.xpath("//input[@id='modelObject_ActiveDutyTo']"))), 5000);
+    driver.executeScript("arguments[0].scrollIntoView(true);", driver.findElement(By.xpath("//input[@id='modelObject_ActiveDutyTo']")));
+    driver.findElement(By.xpath("//input[@id='modelObject_ActiveDutyTo']")).sendKeys("Sep 02, 2007");
+    driver.sleep(500);
+    /*
+    driver.executeScript("arguments[0].scrollIntoView(true);", driver.findElement(homelandDefense));
+    driver.findElement(homelandDefense).click();
+    driver.wait(until.elementIsEnabled(driver.findElement(By.xpath("//input[@id='modelObject_RemainOnHomelandDefense' and @value='False']"))), 5000);
+    driver.sleep(500);
+    driver.executeScript("arguments[0].scrollIntoView(true);", driver.findElement(By.xpath("//input[@id='modelObject_RemainOnHomelandDefense' and @value='False']")));
+    driver.findElement(By.xpath("//input[@id='modelObject_RemainOnHomelandDefense' and @value='False']")).click();
+    driver.wait(until.elementIsEnabled(driver.findElement(By.xpath("//input[@id='modelObject_HomelandDefenseTo']"))), 5000);
+    driver.findElement(By.xpath("//input[@id='modelObject_HomelandDefenseTo']")).sendKeys("Sep 02, 2008");
+    */
+    driver.findElement(saveBtn).click();
+    //req.waitForSuccessMsg();
+    driver.sleep(2000);
     driver.findElement(income).click();
     driver.wait(until.elementLocated(By.xpath("//div[starts-with(@id, 'meansTest')]//tr[@data-budgetitem-type='Employment']")));
     
