0a8a501b2c718e640b391d658bc28d5aacaffe32
Mikhail Terentiev <anhungered@gmail.com>
2016-04-04 19:13:29 +0300

    added mocha

--- tests/crudTasks.js
+++ tests/crudTasks.js
@@ -12,17 +12,34 @@ var webdriver = req.webdriver,
     until = req.until;
 
 var assert = req.assert,
-    fs = req.fs;
+    fs = req.fs,
+    mocha = require('selenium-webdriver/testing');
 
-req.authorize(test.env, test.login, test.password);
-req.closeTabs();
-
-tasks.dashboardTasks();
-req.closeTabs();
-
-req.openCreateContact('dashboard', 'person');
-req.createPerson(test.testPerson);
-tasks.contactTasks();
-req.closeTabs();
-
-req.logOut();
+mocha.describe('TASKS', function() {
+    this.timeout(0);
+    
+    mocha.before(function() {
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(2000);
+        req.authorize(test.env, test.login, test.password);
+        req.catchUncaughtExceptions();
+        req.closeTabs()
+    });
+    
+    mocha.it('CRUD Tasks on Dashboard', function() {
+        tasks.dashboardTasks()
+    });
+    
+    mocha.it('CRUD Tasks in Contacts Events', function() {
+        req.closeTabs();
+        req.openCreateContact('dashboard', 'person');
+        req.createPerson(test.testPerson);
+        tasks.contactTasks()
+    });
+    
+    mocha.after(function() {
+        req.closeTabs();
+        req.logOut()
+    });
+    
+});
